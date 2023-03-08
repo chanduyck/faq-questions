72a5c34e66b8d62debec65d9baf5b6f281989565
chandan <Chandankumaryck21@gmail.com>
2023-03-08 20:07:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import Question from "./Question";
+import "./styles.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App(){
+  return(
+    <>
+    <h1>Project2: faq</h1>
+    <div className="container">
+      <h2>Frequently asked Questions</h2>
+      <div className="questions">
+        {questions.map((question)=>(
+          <Question key={question.id} question={question}/>
+        ))}
+        </div>
     </div>
-  );
+    </>
+  )
+}
+const questions=[
+{
+  id:1,
+  title:"Is this is a good product?",
+  info:"Tell me briefly about the product"
+},
+{
+  id:2,
+  title:"How much does it cost?",
+  info:"Describe the cost of the product"
+},
+{
+  id:3,
+  title:"When can I get it ?",
+  info:"Tell me the delivery date"
 }
 
-export default App;
+];
